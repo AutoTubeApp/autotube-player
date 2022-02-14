3e57daa51c12c66f55179a2bea8ac637e2362a9c
Stéphane Depierrepont <toorop@gmail.com>
2022-02-14 14:20:41 +0100

    IOS compatibility

--- embed.js
+++ embed.js
@@ -2,28 +2,28 @@ import './index.css'
 
 // head
 const scripts = [
-    ['https://cdnjs.cloudflare.com/ajax/libs/shaka-player/3.3.1/shaka-player.compiled.js'],
-    ['https://cdnjs.cloudflare.com/ajax/libs/shaka-player/3.3.1/shaka-player.ui.min.js'],
-    ['https://www.gstatic.com/cv/js/sender/v1/cast_sender.js']
+  ['https://cdnjs.cloudflare.com/ajax/libs/shaka-player/3.3.1/shaka-player.compiled.js'],
+  ['https://cdnjs.cloudflare.com/ajax/libs/shaka-player/3.3.1/shaka-player.ui.min.js'],
+  ['https://www.gstatic.com/cv/js/sender/v1/cast_sender.js']
 ]
 
 const css = [
-    ['https://cdnjs.cloudflare.com/ajax/libs/shaka-player/3.3.1/controls.min.css']
+  ['https://cdnjs.cloudflare.com/ajax/libs/shaka-player/3.3.1/controls.min.css']
 ]
 
 // add scripts
 scripts.forEach(script => {
-    const scriptTag = document.createElement('script')
-    scriptTag.src = script[0]
-    document.head.appendChild(scriptTag)
+  const scriptTag = document.createElement('script')
+  scriptTag.src = script[0]
+  document.head.appendChild(scriptTag)
 })
 
 // add css
 css.forEach(css => {
-    const cssTag = document.createElement('link')
-    cssTag.href = css[0]
-    cssTag.rel = 'stylesheet'
-    document.head.appendChild(cssTag)
+  const cssTag = document.createElement('link')
+  cssTag.href = css[0]
+  cssTag.rel = 'stylesheet'
+  document.head.appendChild(cssTag)
 })
 
 // body
@@ -43,140 +43,149 @@ const body = `
 document.querySelector('#app').innerHTML = body
 
 const posterUrl = "thumbnail.jpg"
-const manifestUrl = "dash.mpd"
 let video
+
+const isIOSDevice = () => {
+  return !!navigator.userAgent && /iPad|iPhone|iPod/.test(navigator.userAgent);
+}
+
+// if MediaSource is not supported, switch to HLS
+const getVideoManifest = () => {
+  return window['MediaSource'] ? 'dash.mpd' : 'master.m3u8'
+}
 
 // shaka player
 const initShakaPlayer = async () => {
-    //console.log('Shake UI loaded')
-    // When using the UI, the player is made automatically by the UI object.
-    video = document.getElementById('video')
-    // set poster
-    video.setAttribute('poster', posterUrl)
-    //video.setAttribute('style', "background-color: black;")
 
-    const ui = video.ui
-    const controls = ui.getControls()
-    const player = controls.getPlayer()
+  //console.log('Shake UI loaded')
+  // When using the UI, the player is made automatically by the UI object.
+  video = document.getElementById('video')
+  // set poster
+  video.setAttribute('poster', posterUrl)
+  //video.setAttribute('style', "background-color: black;")
 
-    // Attach player and ui to the window to make it easy to access in the JS console.
-    window.player = player
-    window.ui = ui
+  const ui = video.ui
+  const controls = ui.getControls()
+  const player = controls.getPlayer()
 
-    // config
-    player.configure({
-        abr: { // let it load a 3500 kbps version first
-            defaultBandwidthEstimate: 10000000
-        },
-        streaming: {
-            bufferingGoal: 4, // small buffers to enable quick quality switch
-            rebufferingGoal: 2,
-            bufferBehind: 20
-        },
-        manifest: {
-            defaultPresentationDelay: 30
-        }
-    })
+  // Attach player and ui to the window to make it easy to access in the JS console.
+  window.player = player
+  window.ui = ui
 
-    // Listen for error events.
-    player.addEventListener('error', onPlayerErrorEvent)
-    player.addEventListener('buffering', (evt) => {
-        //console.log('LoadedEvent')
-        console.log(evt)
-        if (!evt.buffering) {
-            parent.postMessage('att-video-loaded', '*')
-        }
-    })
-    controls.addEventListener('error', onUIErrorEvent)
-
-
-    // Try to load a manifest.
-    // This is an asynchronous process.
-    try {
-        await player.load(manifestUrl)
-        // This runs if the asynchronous load is successful.
-        //console.log('The video has now been loaded!')
-
-        //parent.postMessage('att-video-loaded', '*')
-        // send player size
-        resizePlayer()
-        // init api
-        initApi()
-    } catch (error) {
-        onPlayerError(error)
+  // config
+  player.configure({
+    abr: { // let it load a 3500 kbps version first
+      defaultBandwidthEstimate: 10000000
+    },
+    streaming: {
+      bufferingGoal: 4, // small buffers to enable quick quality switch
+      rebufferingGoal: 2,
+      bufferBehind: 20
+    },
+    manifest: {
+      defaultPresentationDelay: 30
     }
+  })
+
+  // Listen for error events.
+  player.addEventListener('error', onPlayerErrorEvent)
+  player.addEventListener('buffering', (evt) => {
+    if (!evt.buffering) {
+      parent.postMessage('att-video-loaded', '*')
+    }
+  })
+  controls.addEventListener('error', onUIErrorEvent)
+
+
+  // Try to load a manifest.
+  // This is an asynchronous process.
+  try {
+    //this.player.src = getVideoManifest()
+    await player.load(getVideoManifest())
+    // This runs if the asynchronous load is successful.
+    //console.log('The video has now been loaded!')
+    isIOSDevice() && video.play()
+
+    //parent.postMessage('att-video-loaded', '*')
+    // send player size
+    resizePlayer()
+    // init api
+    initApi()
+  } catch (error) {
+    onPlayerError(error)
+  }
 }
 
 const initFailed = (evt) => {
-    // Handle the failure to load; errorEvent.detail.reasonCode has a
-    // shaka.ui.FailReasonCode describing why.
-    console.error('Unable to load the UI library!' + evt.detail)
-    //console.log(evt)
+  // Handle the failure to load; errorEvent.detail.reasonCode has a
+  // shaka.ui.FailReasonCode describing why.
+  console.error('Unable to load the UI library!' + evt.detail)
+  //console.log(evt)
 }
 
 const onPlayerErrorEvent = (evt) => {
-    // Extract the shaka.util.Error object from the event.
-    onPlayerError(evt.detail)
+  // Extract the shaka.util.Error object from the event.
+  onPlayerError(evt.detail)
 }
 
 function onPlayerError(error) {
-    // Handle player error
-    console.error('Error code', error.code, 'object', error)
+  // Handle player error
+  console.error('Error code', error.code, 'object', error)
 }
 
 function onUIErrorEvent(evt) {
-    // Extract the shaka.util.Error object from the event.
-    onPlayerError(evt.detail)
+  // Extract the shaka.util.Error object from the event.
+  onPlayerError(evt.detail)
 }
 
 const resizePlayer = () => {
-    const videoContainer = window.document.querySelector('.shaka-video-container')
-    videoContainer.style.height = `${window.innerHeight}px`
+  const videoContainer = window.document.querySelector('.shaka-video-container')
+  videoContainer.style.height = `${window.innerHeight}px`
 }
 
 const getVideoSize = () => {
-    const videoContainer = window.document.querySelector('.shaka-video-container')
-    return {
-        width: videoContainer.offsetWidth,
-        height: videoContainer.offsetHeight,
-        ratio: videoContainer.offsetWidth / videoContainer.offsetHeight
-    }
+  const videoContainer = window.document.querySelector('.shaka-video-container')
+  return {
+    width: videoContainer.offsetWidth,
+    height: videoContainer.offsetHeight,
+    ratio: videoContainer.offsetWidth / videoContainer.offsetHeight
+  }
 }
 
 const displayControls = (display) => {
-    const controls = window.document.querySelector('.shaka-controls-container')
-    if (display) {
-        controls.setAttribute('shown', 'true')
-    } else {
-        controls.removeAttribute('shown')
-    }
+  const controls = window.document.querySelector('.shaka-controls-container')
+  if (display) {
+    controls.setAttribute('shown', 'true')
+  } else {
+    controls.removeAttribute('shown')
+  }
 }
 
 window.displayControls = displayControls
 const initApi = () => {
 
-    // listen for player size changes
-    window.addEventListener('resize', resizePlayer)
-    // listen from parent
-    window.addEventListener('message', (event) => {
-        console.log('message from parent', event.data)
-        switch (event.data) {
-            case 'att-video-play':
-                video.play()
-                break
-            case 'att-video-size':
-                parent.postMessage(getVideoSize(), '*')
-                break
-            case 'att-controls-display':
-                displayControls(true)
-                break
-            case 'att-controls-hide':
-                displayControls(false)
-                break
-            default:
-                break
-        }
-    })
+  // listen for player size changes
+  window.addEventListener('resize', resizePlayer)
+  // listen from parent
+  window.addEventListener('message', (event) => {
+    console.log('message from parent', event.data)
+    switch (event.data) {
+      case 'att-video-play':
+        video.play()
+        break
+      case 'att-video-size':
+        parent.postMessage(getVideoSize(), '*')
+        break
+      case 'att-controls-display':
+        displayControls(true)
+        break
+      case 'att-controls-hide':
+        displayControls(false)
+        break
+      default:
+        break
+    }
+  })
 }
 
 // shaka player events
--- main.js
+++ main.js
@@ -6,30 +6,47 @@ allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; pic
 `
 
 const getIframe = () => {
-    return window.document.querySelector('#iVideo')
+  return window.document.querySelector('#iVideo')
 }
 
 const getMain = () => {
-    return window.document.querySelector('main')
+  return window.document.querySelector('main')
 }
 
 const getBaseUrl = () => {
-    return window.location.href.split('?', 1)[0].split('/').slice(0, -1).join('/')
+  return window.location.href.split('?', 1)[0].split('/').slice(0, -1).join('/')
+}
+
+/**
+ * String.prototype.replaceAll() polyfill
+ * https://gomakethings.com/how-to-replace-a-section-of-a-string-with-another-one-with-vanilla-js/
+ * @author Chris Ferdinandi
+ * @license MIT
+ */
+if (!String.prototype.replaceAll) {
+  String.prototype.replaceAll = function (str, newStr) {
+    // If a regex pattern
+    if (Object.prototype.toString.call(str).toLowerCase() === '[object regexp]') {
+      return this.replace(str, newStr)
+    }
+    // If a string
+    return this.replace(new RegExp(str, 'g'), newStr)
+  }
 }
 
 const hydrateHtml = () => {
-    const title = window.document.querySelector('title').innerText
-    let embed = embedTemplate.replaceAll('{{baseURL}}', getBaseUrl())
-    embed = embed.replaceAll('{{title}}', title)
-    const dashLink = getBaseUrl() + '/dash.mpd'
-    window.document.querySelector('#embed-code').innerHTML = embed.replaceAll('<', '&lt').replaceAll('>', '&gt')
-    window.document.querySelector('#dash-link').innerHTML = dashLink
+  const title = window.document.querySelector('title').innerText
+  let embed = embedTemplate.replaceAll('{{baseURL}}', getBaseUrl())
+  embed = embed.replaceAll('{{title}}', title)
+  const dashLink = getBaseUrl() + '/dash.mpd'
+  window.document.querySelector('#embed-code').innerHTML = embed.replaceAll('<', '&lt').replaceAll('>', '&gt')
+  window.document.querySelector('#dash-link').innerHTML = dashLink
 }
 
 const handleMessage = (event) => {
-    if (event.data === 'att-video-loaded') {
-        getMain().classList.remove('opacity-0')
-    }
+  if (event.data === 'att-video-loaded') {
+    getMain().classList.remove('opacity-0')
+  }
 }
 // main
 window.addEventListener('message', handleMessage)
@@ -37,12 +54,12 @@ window.addEventListener('message', handleMessage)
 //dark mode
 const selectElement = window.document.querySelector('#toggle-dark-mode')
 selectElement.addEventListener('change', (evt) => {
-    const result = document.querySelector('html')
-    if (evt.target.checked) {
-        result.classList.add('dark')
-    } else {
-        result.classList.remove('dark')
-    }
+  const result = document.querySelector('html')
+  if (evt.target.checked) {
+    result.classList.add('dark')
+  } else {
+    result.classList.remove('dark')
+  }
 })
 
 // hydrate html
